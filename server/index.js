89c8297a98d70c64ad85425855531abdc1febcda
Anthony Riggi <anthonyriggithree@gmail.com>
2019-03-29 20:54:26 -0700

    change route require cors

--- server/index.js
+++ server/index.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const server = express();
+const cors = require('cors');
 const port = 3001;
 const bodyParser = require('body-parser');
 const db = require('../database/index.js');
 
+
 server.use(express.static('./public/dist'));
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded());
+server.use(cors());
 
  
 server.listen(port, () => {
     console.log(`listening @ http://localhost:${port}`);
 });
 
-server.get('/some', (req, res) => {
+server.get('/test', (req, res) => {
   console.log('message from client: a get request has been made');
   res.send('message from server: your get request was successful')
 });
