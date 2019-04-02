6c68a046b6065991d4247d00679fe05d60e01100
Anthony Riggi <anthonyriggithree@gmail.com>
2019-04-02 12:35:03 -0700

    make test async

--- specs/index.test.js
+++ specs/index.test.js
@@ -24,14 +24,18 @@ describe('testing data length with call to api', function() {
         expect(typeof 'this is a string').toBe('string');
         done();
       });
-      test('there should be 100 listings exactly', (done) => {
-        $.ajax({
-          url: 'http://localhost:3001/api/testing',
-          type: 'GET',
-          success: (data) => {
-            expect(data.length).toBe(100);
-            done();
-          }
-        });
+      test('there should be 100 listings exactly', (error, done) => {
+        if (error){
+          done()
+        } else {
+          $.ajax({
+            url: 'http://localhost:3001/api/testing',
+            type: 'GET',
+            success: (data) => {
+              expect(data.length).toBe(100);
+              done();
+            }
+          });   
+        }
       });
 });
