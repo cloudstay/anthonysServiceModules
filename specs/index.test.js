dad7ab3bcd34e6fc6e28be3e80d5c7822bc17208
Anthony Riggi <anthonyriggithree@gmail.com>
2019-04-02 13:14:59 -0700

    fix jest test suite syntax

--- specs/index.test.js
+++ specs/index.test.js
@@ -20,39 +20,35 @@ const $ = require('jquery');
 // });
 
 describe('testing data length with call to api', function() {
-    test('this should pass everytime', (done) => {
+    test('this should pass everytime', () => {
         expect(typeof 'this is a string').toBe('string');
-        done();
       });
-      test('there should be 100 listings exactly', (done) => {
+      test('there should be 100 listings exactly', () => {
         $.ajax({
           url: 'http://localhost:3001/api/testing',
           type: 'GET',
           success: (data) => {
             expect(data.length).toBe(100);
-            done();
           }
         });
       });
 });
 
 describe('testing image url of a random data listing to be in string format', function() {
-  test('this should pass everytime', (done) => {
+  test('this should pass everytime', () => {
       expect(typeof 'this is a string').toBe('string');
-      done();
     });
-    test('a random listings policy diagram image should be stored as a string', (done) => {
+    test('a random listings policy diagram image should be stored as a string', () => {
       $.ajax({
         url: 'http://localhost:3001/api/testing',
         type: 'GET',
         success: (data) => {
           var randomIndex = Math.floor(Math.random() * Math.floor(100));
           expect(typeof data[randomIndex].policyDiagram).toBe('string');
-          done();
         }
       });
     });
-    test('a random listings things to do event image should be stored as a string', (done) => {
+    test('a random listings things to do event image should be stored as a string', () => {
       $.ajax({
         url: 'http://localhost:3001/api/testing',
         type: 'GET',
@@ -60,11 +56,10 @@ describe('testing image url of a random data listing to be in string format', fu
           var randomIndex = Math.floor(Math.random() * Math.floor(100));
           var randomIndexEvents = Math.floor(Math.random() * Math.floor(8));
           expect(typeof data[randomIndex].thingsToDo[randomIndexEvents].eventImg).toBe('string');
-          done();
         }
       });
     });
-    test('a random listings related home image should be stored as a string', (done) => {
+    test('a random listings related home image should be stored as a string', () => {
       $.ajax({
         url: 'http://localhost:3001/api/testing',
         type: 'GET',
@@ -72,7 +67,6 @@ describe('testing image url of a random data listing to be in string format', fu
           var randomIndex = Math.floor(Math.random() * Math.floor(100));
           var randomIndexRelatedHomes = Math.floor(Math.random() * Math.floor(12));
           expect(typeof data[randomIndex].relatedHomes[randomIndexRelatedHomes].listingImg).toBe('string');
-          done();
         }
       });
     });
@@ -80,11 +74,10 @@ describe('testing image url of a random data listing to be in string format', fu
 
 
 describe('testing image urls of a random data listing to have legitimate endpoints to reach api', function() {
-  test('this should pass everytime', (done) => {
-      expect(typeof 'this is a string').toBe('string');
-      done();
+  test('this should pass everytime', () => {
+      expect(typeof 'this is a string').toBe('string')
     });
-    test('a random listings policy diagram image should have an endpoint less then 4', (done) => {
+    test('a random listings policy diagram image should have an endpoint less then 4', () => {
       $.ajax({
         url: 'http://localhost:3001/api/testing',
         type: 'GET',
@@ -92,11 +85,10 @@ describe('testing image urls of a random data listing to have legitimate endpoin
           var randomIndex = Math.floor(Math.random() * Math.floor(100));
           var endpointInt = Number(data[randomIndex].policyDiagram.split('/')[4].split('.')[0]);
           expect(endpointInt < 4).toEqual(true);
-          done();
         }
       });
     });
-    test('a random listings things to do event image should have an endpoint less then 9', (done) => {
+    test('a random listings things to do event image should have an endpoint less then 9', () => {
       $.ajax({
         url: 'http://localhost:3001/api/testing',
         type: 'GET',
@@ -105,11 +97,10 @@ describe('testing image urls of a random data listing to have legitimate endpoin
           var randomIndexEvents = Math.floor(Math.random() * Math.floor(8));
           var endpointInt = Number(data[randomIndex].thingsToDo[randomIndexEvents].eventImg.split('/')[4].split('.')[0]);
           expect(endpointInt < 9).toEqual(true);
-          done();
         }
       });
     });
-    test('a random listings related home image should have an endpoint less then 13', (done) => {
+    test('a random listings related home image should have an endpoint less then 13', () => {
       $.ajax({
         url: 'http://localhost:3001/api/testing',
         type: 'GET',
@@ -118,7 +109,6 @@ describe('testing image urls of a random data listing to have legitimate endpoin
           var randomIndexRelatedHomes = Math.floor(Math.random() * Math.floor(12));
           var endpointInt = Number(data[randomIndex].relatedHomes[randomIndexRelatedHomes].listingImg.split('/')[4].split('.')[0]);
           expect(endpointInt < 13).toEqual(true);
-          done();
         }
       });
     });
