7e98493505a2a72f36d20125e08b1b36bf1dec98
Anthony Riggi <anthonyriggithree@gmail.com>
2019-04-03 13:07:07 -0700

    create and export conponent file

---
+++ client/src/components/TheNeighborhood.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class TheNeighborhood extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+
+    }
+}
+
+    render(){
+      return(
+        <div>TheNeighborhood</div>
+      )
+    }
+}
+
+
+export default TheNeighborhood;
