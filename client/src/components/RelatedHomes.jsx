bd1e95431534da5cb675472bc887a2ed646bf975
Anthony Riggi <anthonyriggithree@gmail.com>
2019-04-03 13:07:00 -0700

    create and export conponent file

---
+++ client/src/components/RelatedHomes.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class RelatedHomes extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+
+    }
+}
+
+    render(){
+      return(
+        <div>RelatedHomes</div>
+      )
+    }
+}
+
+
+export default RelatedHomes;
