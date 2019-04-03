536119ce456428416c892d757a2158dc99fa0be3
Anthony Riggi <anthonyriggithree@gmail.com>
2019-04-03 13:06:45 -0700

    create and export conponent file

---
+++ client/src/components/Communications.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Communications extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+
+    }
+}
+
+    render(){
+      return(
+        <div>Communications</div>
+      )
+    }
+}
+
+
+export default Communications;
