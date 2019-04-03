70d0fef7171e69ce4ee2e025efd6f096a18b4696
Anthony Riggi <anthonyriggithree@gmail.com>
2019-04-03 13:07:11 -0700

    create and export conponent file

---
+++ client/src/components/ThingsToDo.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class ThingsToDo extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+
+    }
+}
+
+    render(){
+      return(
+        <div>ThingsToDo</div>
+      )
+    }
+}
+
+
+export default ThingsToDo;
