04164359873965ec4323f61a4dffac7c02b852c6
Anthony Riggi <anthonyriggithree@gmail.com>
2019-04-03 13:06:39 -0700

    create and export conponent file

---
+++ client/src/components/Cancelations.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Cancelations extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+
+    }
+}
+
+    render(){
+      return(
+        <div>Cancelations</div>
+      )
+    }
+}
+
+
+export default Cancelations;
