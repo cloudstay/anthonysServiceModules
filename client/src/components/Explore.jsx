af30bf92f1e0369a5f792a784049649d9a2459ef
Anthony Riggi <anthonyriggithree@gmail.com>
2019-04-03 13:06:51 -0700

    create and export conponent file

---
+++ client/src/components/Explore.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Explore extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+
+    }
+}
+
+    render(){
+      return(
+        <div>Explore</div>
+      )
+    }
+}
+
+
+export default Explore;
