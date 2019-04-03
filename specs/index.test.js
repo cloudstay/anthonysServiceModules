b04a019825617048ffa448893d7946bd3fbdbd71
Anthony Riggi <anthonyriggithree@gmail.com>
2019-04-02 18:12:49 -0700

    remove/seperate enzyme tests in another directory

--- specs/index.test.js
+++ specs/index.test.js
@@ -1,8 +1,5 @@
 const supertest = require('supertest');
 const $ = require('jquery');
-import React from "react";
-import {shallow} from "enzyme";
-import App from "../client/src/App.jsx";
 
 // describe('service test', function() {
 //     var app = express();
@@ -115,16 +112,4 @@ describe('testing image urls of a random data listing to have legitimate endpoin
         }
       });
     });
-});
-
-
-/////////////////////////////////////////////////////////////////
-////////////////////ENZYME TESTS/////////////////////////////////
-/////////////////////////////////////////////////////////////////
-
-describe('Should render main App component', ()=> {
-  it("should render without error", () => {
-    const wrapper = shallow(<App />);
-    expect((wrapper).exists()).toBe(true);
-  });
 });
