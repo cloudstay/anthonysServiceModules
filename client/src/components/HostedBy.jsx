22b85d14087be76afab19e818114bf1a97a45508
Anthony Riggi <anthonyriggithree@gmail.com>
2019-04-03 13:06:29 -0700

    create and export conponent file

---
+++ client/src/components/HostedBy.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class HostedBy extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+
+    }
+}
+
+    render(){
+      return(
+        <div>Hosted By</div>
+      )
+    }
+}
+
+
+export default HostedBy;
