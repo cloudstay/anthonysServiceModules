15b89d22a1999c875054198afe41b9d803b655ee
Anthony Riggi <anthonyriggithree@gmail.com>
2019-04-03 13:06:56 -0700

    create and export conponent file

---
+++ client/src/components/Policies.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Policies extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+
+    }
+}
+
+    render(){
+      return(
+        <div>Policies</div>
+      )
+    }
+}
+
+
+export default Policies;
