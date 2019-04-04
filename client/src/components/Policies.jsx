fe8d852ddbfc412f4d1250beee36d6b2de30a92b
Anthony Riggi <anthonyriggithree@gmail.com>
2019-04-03 20:30:53 -0700

    render policies component element

--- client/src/components/Policies.jsx
+++ client/src/components/Policies.jsx
@@ -10,7 +10,15 @@ class Policies extends React.Component {
 
     render(){
       return(
-        <div>Policies</div>
+        <div>
+          <h1>Policies</h1>
+          <h3>House Rules</h3>
+          <div>Self Check In: {JSON.stringify(this.props.selfCheckIn)}</div><div>{this.props.checkInTime}</div>
+          <div>{JSON.stringify(this.props.smoking)}</div><div>{JSON.stringify(this.props.parties)}</div>
+          <div>{JSON.stringify(this.props.pets)}</div>
+          <div>{JSON.stringify(this.props.securityDeposit)}</div><div>{this.props.depositAmount}</div>
+          <div>Expand policies button</div>
+        </div>
       )
     }
 }
