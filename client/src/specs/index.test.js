0bfdba5765f729de3d423f697ad4ef985a6c2408
Anthony Riggi <anthonyriggithree@gmail.com>
2019-04-03 21:05:58 -0700

    add test coverage for components rendered on app

--- client/src/specs/index.test.js
+++ client/src/specs/index.test.js
@@ -1,7 +1,18 @@
-//test app
 import React from 'react';
 import { shallow, mount, render } from 'enzyme';
+
 import App from '../components/App.jsx';
+import HostedBy from '../components/HostedBy.jsx';
+import Communications from '../components/Communications.jsx';
+import TheNeighborhood from '../components/TheNeighborhood.jsx';
+import Policies from '../components/Policies.jsx';
+import Cancelations from '../components/Cancelations.jsx';
+import RelatedHomes from '../components/RelatedHomes.jsx';
+import ThingsToDo from '../components/ThingsToDo.jsx';
+import Explore from '../components/Explore.jsx';
+
+// import Listing from '../components/Listing.jsx';
+// import Event from '../components/Event.jsx';
 
 
 describe('render main app component to the DOM', () => {
@@ -10,3 +21,38 @@ describe('render main app component to the DOM', () => {
       expect(wrapper.exists()).toBe(true);
     });
   });
+
+describe('render all imported components within main App component', () => {
+    test("should pass if HostedBy component renders", () => {
+      const wrapper = shallow(<HostedBy/>);
+      expect(wrapper.exists()).toBe(true);
+    });
+    test("should pass if Communications component renders", () => {
+      const wrapper = shallow(<Communications/>);
+      expect(wrapper.exists()).toBe(true);
+    });
+    test("should pass if TheNeighborhood component renders", () => {
+      const wrapper = shallow(<TheNeighborhood/>);
+      expect(wrapper.exists()).toBe(true);
+    });
+    test("should pass if Policies component renders", () => {
+      const wrapper = shallow(<Policies/>);
+      expect(wrapper.exists()).toBe(true);
+    });
+    test("should pass if Cancelations component renders", () => {
+      const wrapper = shallow(<Cancelations/>);
+      expect(wrapper.exists()).toBe(true);
+    });
+    // test("should pass if RelatedHomes component renders", () => {
+    //   const wrapper = shallow(<RelatedHomes/>);
+    //   expect(wrapper.exists()).toBe(true);
+    // });
+    // test("should pass if ThingsToDo component renders", () => {
+    //   const wrapper = shallow(<ThingsToDo/>);
+    //   expect(wrapper.exists()).toBe(true);
+    // });
+    test("should pass if Explore component renders", () => {
+      const wrapper = shallow(<Explore/>);
+      expect(wrapper.exists()).toBe(true);
+    });
+});
