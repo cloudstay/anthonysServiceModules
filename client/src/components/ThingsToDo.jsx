b7e58b530d9e918b03afce4f9d1c76b2ed86f83d
Anthony Riggi <anthonyriggithree@gmail.com>
2019-04-03 20:32:20 -0700

    dynamically render events

--- client/src/components/ThingsToDo.jsx
+++ client/src/components/ThingsToDo.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Event from './Event.jsx';
 
 class ThingsToDo extends React.Component {
   constructor(props){
@@ -9,8 +10,14 @@ class ThingsToDo extends React.Component {
 }
 
     render(){
+      var thingsToDo = this.props.thingsToDo.map((event) => {
+        return <Event eventImg={event.eventImg} eventCategory={event.eventCategory} eventName={event.eventName} price={event.price} ratingAvg={event.ratingAvg} numOfRatings={event.numOfRatings}/>
+      })
       return(
-        <div>ThingsToDo</div>
+        <div>
+          <h1>Things to do near this home!</h1>
+          <ol>{thingsToDo}</ol>
+        </div>
       )
     }
 }
