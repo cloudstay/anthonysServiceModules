a8ed65b0696ef4629e871812261a2725df0ce7db
Anthony Riggi <anthonyriggithree@gmail.com>
2019-04-03 20:29:56 -0700

    render hosted by component element

--- client/src/components/HostedBy.jsx
+++ client/src/components/HostedBy.jsx
@@ -6,11 +6,19 @@ class HostedBy extends React.Component {
     this.state = {
 
     }
-}
+  }
 
     render(){
       return(
-        <div>Hosted By</div>
+        <div>
+          <h2>{this.props.hostName}</h2>
+          <div>{this.props.city}</div><div>{this.props.memberSince}</div>
+          <div>{this.props.numOfReviews}</div><div>{JSON.stringify(this.props.isVerified)}</div>
+          <p>{this.props.hostInfo}</p>
+          <div>{this.props.languages}</div>
+          <div>{this.props.responseTime}</div>
+          <div>{this.props.responseRate}</div>
+        </div>
       )
     }
 }
