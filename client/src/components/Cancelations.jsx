0d405e7290c479da16c63aa8963b789044e3151c
Anthony Riggi <anthonyriggithree@gmail.com>
2019-04-05 23:52:07 -0700

    add conditional rendering

--- client/src/components/Cancelations.jsx
+++ client/src/components/Cancelations.jsx
@@ -4,19 +4,49 @@ class Cancelations extends React.Component {
   constructor(props){
     super(props)
     this.state = {
+      showPanel: false
 
     }
+    this.togglePanel = this.togglePanel.bind(this);
 }
 
+togglePanel(){
+  this.setState({
+    showPanel: this.state.showPanel ? false : true
+  })
+}
+
+
     render(){
-      return(
-        <div>
-          <h1>Cancelations</h1>
-          <div>{this.props.policyDiagram}</div>
-          <div>show more button here</div>
-          <div>get more details button here</div>
-        </div>
-      )
+      if (this.state.showPanel){
+        return(
+          <div>
+            <h2>Cancellations</h2>
+            <div>
+              <img src={this.props.policyDiagram} id='policyDiagram' alt="Policy Diagram"></img>
+            </div>
+            <div className='hostedByVerticalSpacing'>
+              <a className="link"><strong>Get full details</strong></a>
+            </div>
+            <div className='hostedByVerticalSpacing' onClick={() => {this.togglePanel()}}>
+              <a className="link"><strong>Hide policies</strong></a><div id='up'></div>
+            </div>
+            <div className='line-break'></div>
+          </div>
+        )
+      } else {
+        return(
+          <div>
+            <h3>Cancellations</h3>
+            <div>
+            </div>
+            <div className='hostedByVerticalSpacing' onClick={() => {this.togglePanel()}}>
+              <a className="link"><strong>Read more about the policy</strong></a><div id='down'></div>
+            </div>
+            <div className='line-break'></div>
+          </div>
+        )
+      }
     }
 }
 
