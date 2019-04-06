f296e7dd0746d6a4b8b6e0c5a048ef587a44c063
Anthony Riggi <anthonyriggithree@gmail.com>
2019-04-05 23:55:42 -0700

    add conditional render for toggle more info panel

--- client/src/components/TheNeighborhood.jsx
+++ client/src/components/TheNeighborhood.jsx
@@ -8,32 +8,72 @@ class TheNeighborhood extends React.Component {
   constructor(props){
     super(props)
     this.state = {
+      showPanel: false
 
     }
+    this.togglePanel = this.togglePanel.bind(this);
+}
+
+togglePanel(){
+  this.setState({
+    showPanel: this.state.showPanel ? false : true
+  })
 }
 
     render(){
-      return(
-        <div>
-          <div className='line-break'></div>
-          <h2>The Neighborhood</h2>
-          <div>{this.props.city}</div>
-          <p>{this.props.neighborhoodInfo}</p>
-          <div id='map'>
-            <Map google={this.props.google} zoom={14}><Marker onClick={this.onMarkerClick} name={'Current location'} />
-              {/* <InfoWindow onClose={this.onInfoWindowClose}>
-                <div>
-                  <h1>{this.state.selectedPlace.name}</h1>
-                </div>
-              </InfoWindow> */}
-            </Map>
+
+      if (this.state.showPanel){
+        return(
+          <div>
+            <div className='line-break'></div>
+            <h2>The Neighborhood</h2>
+            <div>
+              <p>{this.props.neighborhoodInfo}</p>
+            </div>
+            <div className='hostedByVerticalSpacing' onClick={() => {this.togglePanel()}}>
+              <a className="link"><strong>Hide</strong></a><div id='up'></div>
+            </div>
+            <div id='map'>
+              <Map google={this.props.google} zoom={14}><Marker onClick={this.onMarkerClick} name={'Current location'} />
+                {/* <InfoWindow onClose={this.onInfoWindowClose}>
+                  <div>
+                    <h1>{this.state.selectedPlace.name}</h1>
+                  </div>
+                </InfoWindow> */}
+              </Map>
+            </div>
+            <p>Exact location information is provided after a booking is confirmed.</p>
+            <div className='line-break'></div>
           </div>
-          <p>Exact location information is provided after a booking is confirmed.</p>
-          <div className='line-break'></div>
-        </div>
-      )
+        )
+      } else {
+        return(
+          <div>
+            <div className='line-break'></div>
+            <h2>The Neighborhood</h2>
+            <div className='hostedByVerticalSpacing' onClick={() => {this.togglePanel()}}>
+              <a className="link"><strong>Read more about the neighborhood</strong></a><div id='down'></div>
+            </div>
+            <div id='map'>
+              <Map google={this.props.google} zoom={14}><Marker onClick={this.onMarkerClick} name={'Current location'} />
+                {/* <InfoWindow onClose={this.onInfoWindowClose}>
+                  <div>
+                    <h1>{this.state.selectedPlace.name}</h1>
+                  </div>
+                </InfoWindow> */}
+              </Map>
+            </div>
+            <p>Exact location information is provided after a booking is confirmed.</p>
+            <div className='line-break'></div>
+          </div>
+        )
+      }
     }
 }
+
+
+//city for map use prop
+// {this.props.city}
 
 
 // export default TheNeighborhood;
