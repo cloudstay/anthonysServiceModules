b4d17f697acdc0d13d7bb90ef352324af128924a
Anthony Riggi <anthonyriggithree@gmail.com>
2019-04-05 23:53:02 -0700

    build explore component import random cities

--- client/src/components/Explore.jsx
+++ client/src/components/Explore.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+var faker = require('faker');
 
 class Explore extends React.Component {
   constructor(props){
@@ -11,7 +12,39 @@ class Explore extends React.Component {
     render(){
       return(
         <div>
-          <div>Explore more places in and around {this.props.city}</div>
+          <h2>Explore other options in and around {this.props.city}</h2>
+          <div className='exploreTitleVerticalSpacing'>More places to stay in {this.props.city}: <a className="linkExplore">Houses</a> · <a className="linkExplore">Bed and breakfasts</a> · <a className="linkExplore">Lofts</a> · <a className="linkExplore">Villas</a> · <a className="linkExplore">Condominiums</a></div>
+          <div className='exploreRowContainer'>
+            <div className='exploreHorizontalSpacing'><a className="linkExplore">{faker.address.city()}</a></div>
+            <div className='exploreHorizontalSpacing'><a className="linkExplore">{faker.address.city()}</a></div>
+            <div className='exploreHorizontalSpacing'><a className="linkExplore">{faker.address.city()}</a></div>
+          </div>
+          <div className='exploreRowContainer'>
+            <div className='exploreHorizontalSpacing'><a className="linkExplore">{faker.address.city()}</a></div>
+            <div className='exploreHorizontalSpacing'><a className="linkExplore">{faker.address.city()}</a></div>
+            <div className='exploreHorizontalSpacing'><a className="linkExplore">{faker.address.city()}</a></div>
+          </div>
+          <div className='exploreRowContainer'>
+            <div className='exploreHorizontalSpacing'><a className="linkExplore">{faker.address.city()}</a></div>
+            <div className='exploreHorizontalSpacing'><a className="linkExplore">{faker.address.city()}</a></div>
+            <div className='exploreHorizontalSpacing'><a className="linkExplore">{faker.address.city()}</a></div>
+          </div>
+          <div className='exploreRowContainer'>
+            <div className='exploreHorizontalSpacing'><a className="linkExplore">{faker.address.city()}</a></div>
+            <div className='exploreHorizontalSpacing'><a className="linkExplore">{faker.address.city()}</a></div>
+            <div className='exploreHorizontalSpacing'><a className="linkExplore">{faker.address.city()}</a></div>
+          </div>
+          <div className='exploreRowContainer'>
+            <div className='exploreHorizontalSpacing'><a className="linkExplore">{faker.address.city()}</a></div>
+            <div className='exploreHorizontalSpacing'><a className="linkExplore">{faker.address.city()}</a></div>
+            <div className='exploreHorizontalSpacing'><a className="linkExplore">{faker.address.city()}</a></div>
+          </div>
+          <div className='exploreRowContainer'>
+            <div className='exploreHorizontalSpacing'><a className="linkExplore">{faker.address.city()}</a></div>
+            <div className='exploreHorizontalSpacing'><a className="linkExplore">{faker.address.city()}</a></div>
+            <div className='exploreHorizontalSpacing'><a className="linkExplore">{faker.address.city()}</a></div>
+          </div>
+          <div className='line-break'></div>
         </div>
       )
     }
