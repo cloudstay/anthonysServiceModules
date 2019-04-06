93a4fb3d1b962e2903416af58e94f808747ffcf5
Anthony Riggi <anthonyriggithree@gmail.com>
2019-04-05 18:15:40 -0700

    render map from google api within component

--- client/src/components/TheNeighborhood.jsx
+++ client/src/components/TheNeighborhood.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import apiKeyFile from '../../../googleApiKey.js';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+
+const apiKey = apiKeyFile.apiKey;
 
 class TheNeighborhood extends React.Component {
   constructor(props){
@@ -11,14 +15,29 @@ class TheNeighborhood extends React.Component {
     render(){
       return(
         <div>
-          <h1>The Neighborhood</h1>
+          <div className='line-break'></div>
+          <h2>The Neighborhood</h2>
+          <div>{this.props.city}</div>
           <p>{this.props.neighborhoodInfo}</p>
-          <div>google maps of city: {this.props.city}</div>
-          <p>Exact location shown after booking</p>
+          <div id='map'>
+            <Map google={this.props.google} zoom={14}><Marker onClick={this.onMarkerClick} name={'Current location'} />
+              {/* <InfoWindow onClose={this.onInfoWindowClose}>
+                <div>
+                  <h1>{this.state.selectedPlace.name}</h1>
+                </div>
+              </InfoWindow> */}
+            </Map>
+          </div>
+          <p>Exact location information is provided after a booking is confirmed.</p>
+          <div className='line-break'></div>
         </div>
       )
     }
 }
 
 
-export default TheNeighborhood;
+// export default TheNeighborhood;
+
+export default GoogleApiWrapper({
+  apiKey: (apiKey)
+})(TheNeighborhood)
