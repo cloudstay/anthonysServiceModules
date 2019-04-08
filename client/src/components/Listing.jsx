7dcda96d8face4a403efebc43854cc04993bfbf7
Anthony Riggi <anthonyriggithree@gmail.com>
2019-04-07 18:11:14 -0700

    implement favorite feature for each listing

--- client/src/components/Listing.jsx
+++ client/src/components/Listing.jsx
@@ -4,24 +4,47 @@ class Listing extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-
+      favorite: false
     }
+    this.toggleFavorite = this.toggleFavorite.bind(this);
+}
+
+toggleFavorite(){
+  this.setState({
+    favorite: this.state.favorite ? false : true
+  })
 }
 
     render(){
-      return(
-        <div id='listingContainer'>
-          <div id='listingImgContainer'><img className='listingImg' src={this.props.listingImg}></img></div>
-          <div id='listingInfoContainer'>
-          <div><span id='listingType'><strong>{((this.props.roomArrangement) + ' · ' + (this.props.city)).toUpperCase()}</strong></span></div>
-          <div><strong>{this.props.listingTitle}</strong></div>
-          <div><span id='pricing'>{'$' + this.props.nightlyPrice + ' per night'}</span></div>
-            <div>
-              <div><span id='carouselStar'></span><span id='reviews'><strong>{this.props.numOfReviews}</strong></span></div>
+      if (!this.state.favorite){
+        return(
+          <div id='listingContainer'>
+            <div id='listingImgContainer'><img className='listingImg' src={this.props.listingImg}></img><img onClick={this.toggleFavorite} className='heart' src='../../../public/images/emptyHeart.svg'></img></div>
+            <div id='listingInfoContainer'>
+            <div><span id='listingType'><strong>{((this.props.roomArrangement) + ' · ' + (this.props.city)).toUpperCase()}</strong></span></div>
+            <div><strong>{this.props.listingTitle}</strong></div>
+            <div><span id='pricing'>{'$' + this.props.nightlyPrice + ' per night'}</span></div>
+              <div>
+                <div><span id='carouselStar'></span><span id='reviews'><strong>{this.props.numOfReviews}</strong></span></div>
+              </div>
             </div>
           </div>
-        </div>
-      )
+        )
+      } else {
+        return(
+          <div id='listingContainer'>
+            <div id='listingImgContainer'><img className='listingImg' src={this.props.listingImg}></img><img onClick={this.toggleFavorite} className='heart' src='../../../public/images/redHeart.svg'></img></div>
+            <div id='listingInfoContainer'>
+            <div><span id='listingType'><strong>{((this.props.roomArrangement) + ' · ' + (this.props.city)).toUpperCase()}</strong></span></div>
+            <div><strong>{this.props.listingTitle}</strong></div>
+            <div><span id='pricing'>{'$' + this.props.nightlyPrice + ' per night'}</span></div>
+              <div>
+                <div><span id='carouselStar'></span><span id='reviews'><strong>{this.props.numOfReviews}</strong></span></div>
+              </div>
+            </div>
+          </div>
+        )
+      }
     }
 }
 
