80e4253c37041882b9e70a0b2278e6595d9cfe81
Anthony Riggi <anthonyriggithree@gmail.com>
2019-04-09 14:11:34 -0700

    change to functional stateless component

--- client/src/components/Communications.jsx
+++ client/src/components/Communications.jsx
@@ -1,20 +1,11 @@
 import React from 'react';
 
-class Communications extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {
-
-    }
-}
-
-    render(){
+const Communications = () => {
       return(
         <div>
           <p><strong>Always communicate through CloudStay</strong> · To protect your payment, never transfer money or communicate outside of the CloudStay website or app. <a className="link"><strong>Learn More</strong></a></p>
         </div>
       )
-    }
 }
 
 
